96a8522a2e0279e9198cae5cd5c402fda9c4fb76
Apra Khanna <67959896+ap23hp@users.noreply.github.com>
2022-04-08 14:18:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component, useState } from 'react'
+import {BrowserRouter} from "react-router-dom"
+import Components from './components'
+import {Provider} from "./context"
+
+function App() {
+  const [list,setlist] = useState([])
+
+  const adddetails = (data) => {
+    setlist([...list,data])
+  }
+
+  const deletedata = (data) => {
+    const position = list.findIndex(item => item.id == data.id)
+    const newarray = [...list]
+    newarray.splice(position,1)
+    setlist(newarray)
+  } 
+
+  const savedData = (data) => {
+    const position = list.findIndex(item => item.id == data.id)
+    const newarray = [...list]
+    newarray[position] = data
+    setlist(newarray)
+  }
+  return (
+    <div>
+        <BrowserRouter>
+            <Provider value={{list,adddetails,deletedata,savedData}}>
+              <Components />
+            </Provider>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
